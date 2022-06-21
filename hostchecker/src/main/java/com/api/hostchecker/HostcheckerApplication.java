61ea88870e093180cc0b115d19e01c4153e6c035
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-21 22:43:11 +0900

    Update HostcheckerApplication.java
    사용하지 않는 import 제거

--- hostchecker/src/main/java/com/api/hostchecker/HostcheckerApplication.java
+++ hostchecker/src/main/java/com/api/hostchecker/HostcheckerApplication.java
@@ -1,8 +1,6 @@
 package com.api.hostchecker;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
