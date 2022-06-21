30faf58d8aa6d57e6ef3b3046afe2d1e2b118d51
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-21 22:37:05 +0900

    Update HostcheckerApplication.java
    HostcheckerApplication에 CommandLinerRunner 인터페이스 사용

--- hostchecker/src/main/java/com/api/hostchecker/HostcheckerApplication.java
+++ hostchecker/src/main/java/com/api/hostchecker/HostcheckerApplication.java
@@ -1,13 +1,30 @@
 package com.api.hostchecker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.api.hostchecker.worker.ThreadWorker;
 
 @SpringBootApplication
 public class HostcheckerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HostcheckerApplication.class, args);
+	}
+
+	@Autowired
+	ThreadWorker threadWorker;
+
+	@Bean
+	CommandLineRunner startMonitor() {
+		return args -> {
+			threadWorker.monitor();
+		};
 	}
 
 }
