7b0305601383cd66a46a3df6e15c49f2606abefa
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-21 15:37:47 +0900

    Create HostcheckerApplicationTests.java
    Default test 파일 추가

---
+++ hostchecker/src/test/java/com/api/hostcker/HostcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.hostcker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostcheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
