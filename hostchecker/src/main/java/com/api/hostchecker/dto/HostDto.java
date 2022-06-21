8f7ecb495016434a807399a4f8066676d1f43ec1
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-21 22:37:52 +0900

    Update HostDto.java
    LocalDateTime 타입 관련 필드 추가

--- hostchecker/src/main/java/com/api/hostchecker/dto/HostDto.java
+++ hostchecker/src/main/java/com/api/hostchecker/dto/HostDto.java
@@ -1,12 +1,17 @@
 package com.api.hostchecker.dto;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class HostDto implements Serializable {
 
 	private static final long serialVersionUID = -2659212098742726279L;
 	private String name;
 	private String ip;
+	private LocalDateTime createdDate;
+	private LocalDateTime modifiedDate;
+	private String isAlive;
+	private LocalDateTime lastAliveTime;
 
 	public String getName() {
 		return name;
@@ -22,6 +27,38 @@ public class HostDto implements Serializable {
 
 	public void setIp(String ip) {
 		this.ip = ip;
+	}
+	
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	public String getIsAlive() {
+		return isAlive;
+	}
+
+	public void setIsAlive(String isAlive) {
+		this.isAlive = isAlive;
+	}
+
+	public LocalDateTime getLastAliveTime() {
+		return lastAliveTime;
+	}
+
+	public void setLastAliveTime(LocalDateTime lastAliveTime) {
+		this.lastAliveTime = lastAliveTime;
 	}
 
 }
