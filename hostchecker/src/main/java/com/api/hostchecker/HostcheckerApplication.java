0a289ff5b888659c4e9530e1894f690f51c20e1b
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-21 15:36:30 +0900

    Create HostcheckerApplication.java
    main() 포함된 HostcheckerApplication.java 생성

---
+++ hostchecker/src/main/java/com/api/hostchecker/HostcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.api.hostchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostcheckerApplication.class, args);
+	}
+
+}
