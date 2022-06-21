e21e4f5ee02ec6d119eafe77d7bc957ad2ad1708
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-21 22:35:47 +0900

    Create StatusModel.java
    StatusModel 생성

---
+++ hostchecker/src/main/java/com/api/hostchecker/model/response/StatusModel.java
@@ -0,0 +1,24 @@
+package com.api.hostchecker.model.response;
+
+public class StatusModel {
+
+	private String result;
+	private String name;
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
