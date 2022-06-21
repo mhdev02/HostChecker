2fadc56266568717bf0b26c10d01679283fc82e7
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-21 15:36:47 +0900

    Create HostRest.java
    HostRest 추가

---
+++ hostchecker/src/main/java/com/api/hostchecker/model/response/HostRest.java
@@ -0,0 +1,44 @@
+package com.api.hostchecker.model.response;
+
+import java.time.LocalDateTime;
+
+public class HostRest {
+
+	private String name;
+	private String ip;
+	private LocalDateTime createdDate;
+	private LocalDateTime modifiedDate;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+}
