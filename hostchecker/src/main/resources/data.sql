3af545444871068d7f8fd0552c47c3c15102516e
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-24 13:19:31 +0900

    Update data.sql
    data.sql에 update, delete, insert 구문 추가

--- hostchecker/src/main/resources/data.sql
+++ hostchecker/src/main/resources/data.sql
@@ -1,2 +1,6 @@
 insert into hosts (name, ip) values ('naver', '223.130.195.200');
 insert into hosts (name, ip) values ('google', '142.250.207.46');
+
+update hosts SET name='naver1' WHERE name='naver';
+delete from hosts where name='naver1';
+insert into hosts (name, ip) values ('naver', '223.130.195.200');
