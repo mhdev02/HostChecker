1fb19b0ab4a79e749bd8174e53575e70a0881e9a
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-24 21:26:42 +0900

    Update HostRepository.java
    @Query의 nativeQuery 사용

--- hostchecker/src/main/java/com/api/hostchecker/repository/HostRepository.java
+++ hostchecker/src/main/java/com/api/hostchecker/repository/HostRepository.java
@@ -2,16 +2,20 @@ package com.api.hostchecker.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.api.hostchecker.entity.HostEntity;
 
 public interface HostRepository extends CrudRepository<HostEntity, Long> {
-	
+
 	List<HostEntity> findAll();
 
 	HostEntity findByIp(String ip);
-	
+
 	HostEntity findByName(String name);
+
+	@Query(value = "select * from hosts order by modified_date", nativeQuery = true)
+	List<HostEntity> getAllOrderedByModifiedDate();
 
 }
