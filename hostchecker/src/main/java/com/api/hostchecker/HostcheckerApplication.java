eec21a9834d9f08dd03e97e35ef1df295a70c221
enginedevp <62423408+enginedevp@users.noreply.github.com>
2022-06-24 13:20:06 +0900

    Update HostcheckerApplication.java
    BaseTimeEntity 사용을 위해 @EnableJpaAuditing 추가

--- hostchecker/src/main/java/com/api/hostchecker/HostcheckerApplication.java
+++ hostchecker/src/main/java/com/api/hostchecker/HostcheckerApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.api.hostchecker.worker.ThreadWorker;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HostcheckerApplication {
 
